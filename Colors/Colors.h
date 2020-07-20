1bdb61c93ef0638a18d32faf2c899c8e1c931c12
Joe Masilotti <joe@masilotti.com>
2020-07-20 13:49:04 -0400

    Initial Commit

---
+++ Colors/Colors.h
@@ -0,0 +1,19 @@
+//
+//  Colors.h
+//  Colors
+//
+//  Created by Joe Masilotti on 7/20/20.
+//  Copyright © 2020 Masilotti.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Colors.
+FOUNDATION_EXPORT double ColorsVersionNumber;
+
+//! Project version string for Colors.
+FOUNDATION_EXPORT const unsigned char ColorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Colors/PublicHeader.h>
+
+
